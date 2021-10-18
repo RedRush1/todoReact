f837fcfc0ab82287c16f7d0bd24e5e9253a77860
BasilRebresh <rebresh58gmail.com>
2021-10-18 22:01:19 +0300

    finished Form, updated App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from "react";
 
 import './App.css';
-import React from "react";
+//Importing Components
+import Form from "./components/Form";
 
 function App() {
   return (
       <div className="App">
           <header>
-            <h1>React check git hub</h1>
+            <h1>Today's schedule</h1>
           </header>
+          <Form/>
       </div>
   );
 }
