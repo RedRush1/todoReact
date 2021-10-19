cae50d7cd0b401c73aac87ab8b227c7dbabc2e4e
BasilRebresh <rebresh58gmail.com>
2021-10-19 23:45:54 +0300

    finish todo

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -6,7 +6,9 @@ const ToDoList =  ( {todos} ) => {
     return(
         <div className="todo-container">
             <ul className="todo-list">
-
+                {todos.map( todo => (
+                    <Todo key={todo.id} text={todo.text} />
+                ))}
             </ul>
         </div>
     );
