50762958b22ff2ae892d4c7729590657b54ed414
BasilRebresh <rebresh58gmail.com>
2021-10-21 18:16:50 +0300

    localStorage

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,17 +20,18 @@ const Form = ({inputText, setInputText, todos ,setTodos, setStatus}) => {
 
     return(
         <form>
-            <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>
+            {/*<input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>*/}
             <button onClick={submitTodoHandler} className="todo-button" type="submit">
+                <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input"/>
                 <i className="fas fa-plus-square"></i>
             </button>
-            <div className="select">
-                <select onChange={statusHandler} name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div>
+            {/*<div className="select">*/}
+            {/*    <select onChange={statusHandler} name="todos" className="filter-todo">*/}
+            {/*        <option value="all">All</option>*/}
+            {/*        <option value="completed">Completed</option>*/}
+            {/*        <option value="uncompleted">Uncompleted</option>*/}
+            {/*    </select>*/}
+            {/*</div>*/}
         </form>
     );
 };
